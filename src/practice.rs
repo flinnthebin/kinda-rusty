3ecb64864488ff187eed50beabf10853372514e8
chris flinn <christopher.flinn@gmail.com>
2025-05-08 17:58:12 +1000

    update

--- src/practice.rs
+++
@@ -1,8 +0,0 @@
-use std::thread;
-pub fn move_thread() {
-    let data = vec![1, 2, 3];
-    let handle = thread::spawn(move || {
-        println!("data = {:?}", data);
-    });
-    handle.join().unwrap();
-}
