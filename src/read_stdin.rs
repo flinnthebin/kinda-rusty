1e5f0aca6093569eb83f91e5b63c824efd2fed21
chris flinn <christopher.flinn@gmail.com>
2025-05-12 18:18:56 +1000

    reader

--- src/read_stdin.rs
+++ src/read_stdin.rs
@@ -10,6 +10,34 @@ macro_rules! read_stdin {
     }};
 }
 
+macro_rules! read_lines {
+    () => {{
+        use std::io::{self, BufRead};
+        let stdin = io::stdin();
+        let lines = stdin.lock().lines();
+        lines.map(|l| l.unwrap()).collect::<Vec<String>>()
+    }};
+}
+
+macro_rules! read_tuples {
+    ( $( $t:ty ),+ ) => {{
+        use std::io::Read;
+        let mut buffer = String::new();
+        std::io::stdin().read_to_string(&mut buffer).unwrap();
+        let mut iter = buffer.split_whitespace();
+        let mut v = Vec::new();
+        while let Some(first) = iter.next() {
+            let mut tuple = Vec::new();
+            tuple.push(first.parse::<$($t),+>().unwrap());
+            for _ in 1..($($t)+) {
+                tuple.push(iter.next().unwrap().parse::<$t>().unwrap())
+            }
+            v.push(tuple);
+        }
+        v
+    }};
+}
+
 pub fn reader() {
     let v: Vec<i32> = read_stdin!(i32);
     println!("{:?}", v);
