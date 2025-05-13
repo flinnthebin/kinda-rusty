e0d2309f75da58f5af19600ae5686ff96d74e2e2
chris flinn <christopher.flinn@gmail.com>
2025-05-13 19:34:44 +1000

    update

--- src/practice.rs
+++ src/practice.rs
@@ -10,7 +10,29 @@ macro_rules! read_stdin {
     }};
 }
 
-pub fn reader() {
-    let v: Vec<i32> = read_stdin!(i32);
-    println!("{:?}", v);
+macro_rules! read_lines {
+    ($t:ty) => {{
+        use std::io::{self, BufRead};
+        let stdin = io::stdin();
+        let lines = stdin.lock().lines();
+        lines.map(|l| l.unwrap()).collect::<Vec<String>>()
+    }};
+}
+
+macro_rules! read_triples {
+    ($t1:ty, $t2:ty, $t3:ty) => {{
+        use std::io::Read;
+        let mut buffer = String::new();
+        std::io::stdin().read_to_string(&mut buffer).unwrap();
+        let mut iter = buffer.split_whitespace();
+        let mut v = Vec::new();
+        while let (Some(x), Some(y), Some(z)) = (iter.next(), iter.next(), iter.next()) {
+            v.push((
+                x.parse::<$t1>().unwrap(),
+                y.parse::<$t2>().unwrap(),
+                z.parse::<$t3>().unwrap(),
+            ));
+        }
+        v
+    }};
 }
